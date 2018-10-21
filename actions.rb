4ae9f7794668fb887653ca423299eeb02fb40e43
DX37 <asusx2@mail.ru>
2018-10-21 12:55:50 +0700

    add empty action and test for go_to_work

---
+++ actions.rb
@@ -0,0 +1,29 @@
+class Actions
+  def go_to_work(human)
+    #
+  end
+
+  # def watch_nature(human)
+  #   #
+  # end
+
+  # def netflix_n_wine(human)
+  #   #
+  # end
+
+  # def go_to_bar(human)
+  #   #
+  # end
+
+  # def drink_with_marginals(human)
+  #   #
+  # end
+
+  # def sing_in_subway(human)
+  #   #
+  # end
+
+  # def sleep(human)
+  #   #
+  # end
+end
--- tests/test_actions.rb
+++ tests/test_actions.rb
@@ -4,39 +4,47 @@ require "test/unit"
 
 class ContTest < Test::Unit::TestCase
   def setup
-    val = Valera.new
-    val.health = 100
-    val.mana = 0
-    val.cheerfulness = 0
-    val.tired = 0
-    val.money = 0
+    @act = Actions.new
+    @val = Valera.new
   end
 
   def test_01_go_to_work
-    #
+    @val.health = 100
+    @val.mana = 50
+    @val.cheerfulness = 10
+    @val.tired = 10
+    @val.money = 20
+
+    @act.go_to_work(@val)
+
+    assert_equal 100, @val.health
+    assert_equal 20, @val.mana
+    assert_equal 5, @val.cheerfulness
+    assert_equal 80, @val.tired
+    assert_equal 120, @val.money
   end
 
-  def test_02_watch_nature
-    #
-  end
+  # def test_02_watch_nature
+  #   act.watch_nature(val)
+  # end
 
-  def test_03_netflix_n_wine
-    #
-  end
+  # def test_03_netflix_n_wine
+  #   act.netflix_n_wine(val)
+  # end
 
-  def test_04_go_to_bar
-    #
-  end
+  # def test_04_go_to_bar
+  #   act.go_to_bar(val)
+  # end
 
-  def test_05_drink_with_marginals
-    #
-  end
+  # def test_05_drink_with_marginals
+  #   act.drink_with_marginals(val)
+  # end
 
-  def test_06_sing_in_subway
-    #
-  end
+  # def test_06_sing_in_subway
+  #   act.sing_in_subway(val)
+  # end
 
-  def test_07_sleep
-    #
-  end
+  # def test_07_sleep
+  #   act.sleep(val)
+  # end
 end
