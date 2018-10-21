082057a187dedfeae4dc05647d0b3987f255e328
DX37 <asusx2@mail.ru>
2018-10-21 13:07:46 +0700

    describe go_to_work action

--- actions.rb
+++ actions.rb
@@ -1,6 +1,12 @@
 class Actions
   def go_to_work(human)
-    #
+    human.cheerfulness -= 5
+    human.cheerfulness = -10 if human.cheerfulness < -10
+    human.mana -= 30
+    human.mana = 0 if human.mana < 0
+    human.money += 100
+    human.tired += 70
+    human.tired = 100 if human.tired > 100
   end
 
   # def watch_nature(human)
