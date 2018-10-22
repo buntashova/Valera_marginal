f11ae1c932b3413697f6b963fa23aa2096ccd104
DX37 <asusx2@mail.ru>
2018-10-22 12:18:37 +0700

    rewrite test_actions to rspec

--- spec/test_actions.rb
+++ spec/test_actions.rb
@@ -1,146 +1,152 @@
-require "test/unit"
-require_relative "../valera.rb"
-require_relative "../actions.rb"
+require_relative "../lib/valera.rb"
+require_relative "../lib/actions.rb"
 
-class ActionsTest < Test::Unit::TestCase
-  def setup
-    @act = Actions.new
-    @val = Valera.new
+RSpec.describe Actions do
+  act = Actions.new
+
+  it "go to work" do
+    val = Valera.new
+    val.cheerfulness = 10
+    val.mana = 50
+    val.money = 20
+    val.tired = 10
+
+    act.go_to_work(val)
+
+    expect(val.cheerfulness).to eq 5
+    expect(val.mana).to eq 20
+    expect(val.money).to eq 120
+    expect(val.tired).to eq 80
   end
 
-  def test_01_go_to_work
-    @val.cheerfulness = 10
-    @val.mana = 50
-    @val.money = 20
-    @val.tired = 10
+  it "watch nature" do
+    val = Valera.new
+    val.cheerfulness = 8
+    val.mana = 50
+    val.tired = 10
 
-    @act.go_to_work(@val)
+    act.watch_nature(val)
 
-    assert_equal 5, @val.cheerfulness
-    assert_equal 20, @val.mana
-    assert_equal 120, @val.money
-    assert_equal 80, @val.tired
+    expect(val.cheerfulness).to eq 9
+    expect(val.mana).to eq 40
+    expect(val.tired).to eq 20
   end
 
-  def test_02_watch_nature
-    @val.cheerfulness = 8
-    @val.mana = 50
-    @val.tired = 10
+  it "netflix 'n' wine" do
+    val = Valera.new
+    val.cheerfulness = 10
+    val.mana = 50
+    val.tired = 10
+    val.health = 90
+    val.money = 70
 
-    @act.watch_nature(@val)
+    act.netflix_n_wine(val)
 
-    assert_equal 9, @val.cheerfulness
-    assert_equal 40, @val.mana
-    assert_equal 20, @val.tired
+    expect(val.cheerfulness).to eq 9
+    expect(val.mana).to eq 80
+    expect(val.tired).to eq 20
+    expect(val.health).to eq 85
+    expect(val.money).to eq 50
   end
 
-  def test_03_netflix_n_wine
-    @val.cheerfulness = 10
-    @val.mana = 50
-    @val.tired = 10
-    @val.health = 90
-    @val.money = 70
+  it "go to bar" do
+    val = Valera.new
+    val.cheerfulness = 5
+    val.mana = 0
+    val.tired = 10
+    val.health = 90
+    val.money = 120
 
-    @act.netflix_n_wine(@val)
+    act.go_to_bar(val)
 
-    assert_equal 9, @val.cheerfulness
-    assert_equal 80, @val.mana
-    assert_equal 20, @val.tired
-    assert_equal 85, @val.health
-    assert_equal 50, @val.money
+    expect(val.cheerfulness).to eq 6 # 6, val.cheerfulness
+    expect(val.mana).to eq 60 # 60, val.mana
+    expect(val.tired).to eq 50 # 50, val.tired
+    expect(val.health).to eq 80 # 80, val.health
+    expect(val.money).to eq 20 # 20, val.money
   end
 
-  def test_04_go_to_bar
-    @val.cheerfulness = 5
-    @val.mana = 0
-    @val.tired = 10
-    @val.health = 90
-    @val.money = 120
+  it "drink with marginals" do
+    val = Valera.new
+    val.cheerfulness = 5
+    val.mana = 0
+    val.tired = 10
+    val.health = 90
+    val.money = 900
 
-    @act.go_to_bar(@val)
+    act.drink_with_marginals(val)
 
-    assert_equal 6, @val.cheerfulness
-    assert_equal 60, @val.mana
-    assert_equal 50, @val.tired
-    assert_equal 80, @val.health
-    assert_equal 20, @val.money
+    expect(val.cheerfulness).to eq 10 # 10, val.cheerfulness
+    expect(val.mana).to eq 90 # 90, val.mana
+    expect(val.tired).to eq 90 # 90, val.tired
+    expect(val.health).to eq 10 # 10, val.health
+    expect(val.money).to eq 750 # 750, val.money
   end
 
-  def test_05_drink_with_marginals
-    @val.cheerfulness = 5
-    @val.mana = 0
-    @val.tired = 10
-    @val.health = 90
-    @val.money = 900
+  it "sing in subway" do
+    val = Valera.new
+    val.cheerfulness = 5
+    val.mana = 0
+    val.tired = 10
+    val.money = 200
 
-    @act.drink_with_marginals(@val)
+    act.sing_in_subway(val)
 
-    assert_equal 10, @val.cheerfulness
-    assert_equal 90, @val.mana
-    assert_equal 90, @val.tired
-    assert_equal 10, @val.health
-    assert_equal 750, @val.money
+    expect(val.cheerfulness).to eq 6 # 6, val.cheerfulness
+    expect(val.mana).to eq 10 # 10, val.mana
+    expect(val.tired).to eq 30 # 30, val.tired
+    expect(val.money).to eq 210 # 210, val.money
   end
 
-  def test_06_sing_in_subway
-    @val.cheerfulness = 5
-    @val.mana = 0
-    @val.tired = 10
-    @val.money = 200
+  it "sing in subway drunk" do
+    val = Valera.new
+    val.cheerfulness = 5
+    val.mana = 50
+    val.tired = 10
+    val.money = 200
 
-    @act.sing_in_subway(@val)
+    act.sing_in_subway(val)
 
-    assert_equal 6, @val.cheerfulness
-    assert_equal 10, @val.mana
-    assert_equal 30, @val.tired
-    assert_equal 210, @val.money
+    expect(val.cheerfulness).to eq 6 # 6, val.cheerfulness
+    expect(val.mana).to eq 60 # 60, val.mana
+    expect(val.tired).to eq 30 # 30, val.tired
+    expect(val.money).to eq 260 # 260, val.money
   end
 
-  def test_07_sing_in_subway_drunk
-    @val.cheerfulness = 5
-    @val.mana = 50
-    @val.tired = 10
-    @val.money = 200
+  it "sleep sober" do
+    val = Valera.new
+    val.health = 5
+    val.mana = 20
+    val.tired = 90
 
-    @act.sing_in_subway(@val)
+    act.sleep(val)
 
-    assert_equal 6, @val.cheerfulness
-    assert_equal 60, @val.mana
-    assert_equal 30, @val.tired
-    assert_equal 260, @val.money
+    expect(val.health).to eq 95 # 95, val.health
+    expect(val.mana).to eq 0 # 0, val.mana
+    expect(val.tired).to eq 20 # 20, val.tired
   end
 
-  def test_08_sleep_sober
-    @val.health = 5
-    @val.mana = 20
-    @val.tired = 90
+  it "sleep drunk" do
+    val = Valera.new
+    val.mana = 60
+    val.tired = 90
 
-    @act.sleep(@val)
+    act.sleep(val)
 
-    assert_equal 95, @val.health
-    assert_equal 0, @val.mana
-    assert_equal 20, @val.tired
+    expect(val.mana).to eq 10 # 10, val.mana
+    expect(val.tired).to eq 20 # 20, val.tired
   end
 
-  def test_09_sleep_drunk
-    @val.mana = 60
-    @val.tired = 90
+  it "sleep very drunk" do
+    val = Valera.new
+    val.cheerfulness = 5
+    val.mana = 80
+    val.tired = 90
 
-    @act.sleep(@val)
+    act.sleep(val)
 
-    assert_equal 10, @val.mana
-    assert_equal 20, @val.tired
-  end
-
-  def test_10_sleep_very_drunk
-    @val.cheerfulness = 5
-    @val.mana = 80
-    @val.tired = 90
-
-    @act.sleep(@val)
-
-    assert_equal 2, @val.cheerfulness
-    assert_equal 30, @val.mana
-    assert_equal 20, @val.tired
+    expect(val.cheerfulness).to eq 2 # 2, val.cheerfulness
+    expect(val.mana).to eq 30 # 30, val.mana
+    expect(val.tired).to eq 20 # 20, val.tired
   end
 end
